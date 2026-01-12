883f312be15b176d70b83ad1ad9989d6d2829909
ayeshaababar18-glitch <ayeshaababar18@gmail.com>
2026-01-12 08:51:34 +0500

    Update lab.java

--- lab.java
+++ lab.java
@@ -1,5 +1,8 @@
-public class HelloWorld {
+public class AddNumbers {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
     }
 }
