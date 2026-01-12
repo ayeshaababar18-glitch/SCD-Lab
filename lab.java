2c647abe148df4f834ec982d20358cd90d0c4cf1
ayeshaababar18-glitch <ayeshaababar18@gmail.com>
2026-01-12 08:56:56 +0500

    Change variable 'a' from 5 to 8

--- lab.java
+++ lab.java
@@ -1,6 +1,6 @@
 public class AddNumbers {
     public static void main(String[] args) {
-        int a = 5;
+        int a = 8;
         int b = 7;
         int sum = a + b;
         System.out.println("Sum: " + sum);
